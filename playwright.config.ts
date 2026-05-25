054d23783edfb365ae3e6fc2ba7eddfcb6f45db8
Fode-Zoto <contact@zotomatise.com>
2026-05-25 16:49:03 +0200

    feat(ci): add allure-report Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         //  Mode CI — lisible par machine + partageable
         ["list"],
         ["html", { open: "never", outputFolder: "playwright-report" }], // 📦 artefact CI
+        ["allure-playwright", { resultsDir: "allure-results" }],
         ["junit", { outputFile: "reports/results.xml" }],
         ["blob"], // pour merger les shards en M9.L2
         ["github"], //  annotations dans les Pull Requests GitHub
